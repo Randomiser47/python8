b14f1faccfabe35614a488a9839184b0f079b92d
Hassaan Alvi <110996624+Randomiser47@users.noreply.github.com>
2025-01-14 21:43:36 +0500

    Add files via upload

---
+++ day8/function_with_more_inputs.py
@@ -0,0 +1,5 @@
+def greet_with(name,location):
+    print(f"Hi my name is {name}")
+    print(f"I am from {location}")
+
+greet_with(name ="Hassaan", location= "Melbourne")
---
+++ day8/greet.py
@@ -0,0 +1,12 @@
+def greet():
+    print("Aslam-o-Alikum")
+    print("My name is Hassaan")
+    print("how are you doing?")
+
+greet()
+
+def greet_with_name(name):
+    print(f"Hi {name}")
+    print(f"how are you doing {name}")
+
+greet_with_name(123)
